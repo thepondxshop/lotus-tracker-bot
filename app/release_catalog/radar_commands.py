903893e62e37bab698dd4c6048e9d0b67009657e
thepondxshop <thepondxshop@gmail.com>
2026-09-26 03:41:07 -0400

    Update radar_commands.py

--- app/release_catalog/radar_commands.py
+++ app/release_catalog/radar_commands.py
@@ -10,7 +10,7 @@ from .commands import safe
 from .extraction import GAMES
 from .radar import ReleaseRadar
 from .radar_diagnostics import RadarDiagnostics
-VERSION = "1.6.4"
+VERSION = "1.6.5"
 from .service import CatalogError
 from .source_confidence import confidence
 
