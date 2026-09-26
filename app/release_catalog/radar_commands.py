f6860e0ca39477d45d78266b17ae9fa118700b0c
thepondxshop <thepondxshop@gmail.com>
2026-09-26 02:54:21 -0400

    Bump version from 1.6.3 to 1.6.4

--- app/release_catalog/radar_commands.py
+++ app/release_catalog/radar_commands.py
@@ -10,7 +10,7 @@ from .commands import safe
 from .extraction import GAMES
 from .radar import ReleaseRadar
 from .radar_diagnostics import RadarDiagnostics
-VERSION = "1.6.3"
+VERSION = "1.6.4"
 from .service import CatalogError
 from .source_confidence import confidence
 
